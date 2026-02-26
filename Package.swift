e51f0d3e443e39533cb6b16009d93c0636923ee6
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-02-26 15:30:51 +0200

    min iOS version v17

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CBORSwift",
-    platforms: [.macOS(.v10_15), .iOS(.v14), .watchOS(.v9)],
+    platforms: [.macOS(.v10_15), .iOS(.v17), .watchOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
